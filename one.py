d089b72050709219117443e74d5a582c3c7e696f
Wesley Hoy <whoy@ey3.tech>
2023-03-04 13:56:04 -0500

    More descriptive

--- one.py
+++ one.py
@@ -4,13 +4,12 @@ us_data = [369, 423, 416, 750, 928]
 
 us_rateOfChange = []
 
+# Find rate of change between each data point
 for i in range(len(us_data)):
     if not len(us_data) - 1 <= i:
         us_rateOfChange.append(us_data[i + 1] / us_data[i])
-        print(us_rateOfChange)
 
-print("Finished: " + str(us_rateOfChange))
-
+# Find an average of the rate of change
 us_average = 0
 
 for x in us_rateOfChange:
@@ -18,7 +17,7 @@ for x in us_rateOfChange:
 
 us_average = us_average / (len(us_data) - 1)
 
-print("Average rate of change (in 1000s): " + str(us_average))
+print("US Average rate of change (in 1000s): " + str(us_average))
 
 # United Kingdom - DATA NEEDED
 
@@ -29,9 +28,6 @@ uk_rateOfChange = []
 for i in range(len(uk_data)):
     if not len(uk_data) - 1 <= i:
         uk_rateOfChange.append(uk_data[i + 1] / uk_data[i])
-        print(uk_rateOfChange)
-
-print("Finished: " + str(uk_rateOfChange))
 
 uk_average = 0
 
@@ -40,4 +36,4 @@ for x in uk_rateOfChange:
 
 uk_average = uk_average / (len(uk_data) - 1)
 
-print("Average rate of change (in 1000s): " + str(us_average))
+print("UK Average rate of change (in 1000s): " + str(us_average))
