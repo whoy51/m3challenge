d205e450c18e22c8fb272e597933f5c4e060b94a
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2023-03-04 15:10:53 -0500

    updated code

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template
-from one import us_predict
+#!/usr/bin/python3
 
-app = Flask(__name__)
+# from flask import Flask, render_template
+import data
 
 
-@app.route('/')
+# app = Flask(__name__)
+
+
+# @app.route('/')
 def index():
-    print("Hello World!")
-    print(str(us_predict))
-    return render_template('index.html', data=us_predict)
+    print("US prediction: " + str(data.us_prediction))
+    print("US Average rate of change (in 1000s): " + str(data.us_average))
+    print("")
+    print("UK prediction: " + str(data.uk_prediction))
+    print("UK Average rate of change (in 1000s): " + str(data.uk_average))
+    # return render_template('index.html', data=data.uk_prediction)
+
 
 
 if __name__ == '__main__':
-    app.run()
+    index()
+    # app.run()
---
+++ m3.py
@@ -0,0 +1,25 @@
+# Finds the rate of change between each data point in _data array and returns
+# an array of the calculated rates
+def roc(_data: list) -> list:
+    rates: list = []
+    for i in range(len(_data)):
+        if not len(_data) - 1 <= i:
+            rates.append(_data[i + 1] / _data[i])
+    return rates
+
+# Calculates the average rate of change for a list of rates _data and returns it
+def average_roc(_data: list, _len: int) -> float:
+    average = 0
+    for value in _data:
+        average += value
+    average /= _len
+    return average
+
+
+def predict(_data: list, _average: float):
+    # print("we do something")
+    data = [_data[0]]
+
+    for i in range(10):
+        data.append(data[0] * (_average ** i))
+    return data
